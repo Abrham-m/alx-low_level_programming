68421209984da3ea26a4c216c04063227e2503b3
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-03 14:15:24 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
-int main()
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int main(void)
 {
-        puts("Programming is like building a multilingual puzzle\n");
-        return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
