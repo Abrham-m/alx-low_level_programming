38f29ae1c3e92193fa725e3160ff00f07c3c97aa
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-06 05:00:27 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * jack_bauer - ;ljsjfjas
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int hour = 0;
+	int minute = 0;
+
+	for (; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+			printf("%02d:%02d\n", hour, minute);
+	}
+}
