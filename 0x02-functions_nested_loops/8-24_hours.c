2498460d7f27ba020d50bf805ff0e0d3828a1431
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-06 08:24:18 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,39 +14,31 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
-			if (hour < 10 )
+			if (hour < 10)
 			{
-				if (minute < 10)
-				{
-					_putchar('0');
-					_putchar(hour);
-					_putchar(':');
-					_putchar('0');
-					_putchar(minute);
-				}
-				else
-				{
-					_putchar('0');
-					_putchar(hour);
-					_putchar(':');
-					_putchar(minute);
-				}
+				_putchar('0');
+				_putchar(hour + '0');
+				_putchar('\n');
 			}
-			else
+			else if (hour >= 10)
 			{
-				if (minute < 10)
-				{
-					_putchar(hour);
-					_putchar(':');
-					_putchar('0');
-					_putchar(minute);
-				}
-				else
-				{
-					_putchar(hour);
-					_putchar(':');
-					_putchar(minute);
-				}
+				_putchar((hour / 10) + '0');
+				_putchar((hour % 10) + '0');
+				_putchar('\n');
+			}
+			if (minute < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(minute + '0');
+				_putchar('\n');
+			}
+			else if (minute >= 10)
+			{
+				_putchar(':');
+				_putchar((minute / 10) + '0');
+				_putchar((minute % 10) + '0');
+				_putchar('\n');
 			}
 		}
 	}
