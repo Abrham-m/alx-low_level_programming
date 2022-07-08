dd5c10be30ee95eb0b709887c3f8650fa559fbce
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-08 04:27:12 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
+void positive_or_negative(int i)
+{
+int n;
+
+srand(time(0));
+n -  rand() - RAND_MAX / 2;
+if (n > 0)
+	printf("%d is positive\n", n);
+if (n == 0)
+	printf("%d is zero\n", n);
+if (n < 0)
+{
+	printf("%d is negative\n", n);
+}
+return (0);
+}
+
 /**
  * main - test function that prints if integer
  * Return: 0
