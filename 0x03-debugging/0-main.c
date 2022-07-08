cf4773b1d5521c5e7ef714dc4f0990dae7612850
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-08 12:18:42 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,9 @@
 #include "main.h"
 
-void positive_or_negative(int i)
-{
-int n;
-
-srand(time(0));
-n = i;
-if (n > 0)
-	printf("%d is positive\n", n);
-if (n == 0)
-	printf("%d is zero\n", n);
-if (n < 0)
-{
-	printf("%d is negative\n", n);
-}
-return (0);
-}
-
 /**
  * main - test function that prints if integer
  * Return: 0
+ * positive_or_negative(int i) - djfs
  */
 
 int main(void)
@@ -30,5 +14,23 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
+
+}
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+
 
 }
