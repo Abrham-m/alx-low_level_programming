b796a35863d751719520109da6db8a70cc6fbddb
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-10 13:34:09 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - is a function that return a product of two variables
+ * @a: is the first variable
+ * @b: is the second varible
+ * Return: an integer
+ */
+
+int mul(int a, int b)
+{
+	int res ;
+
+	res = a * b;
+
+	return (res);
+}
