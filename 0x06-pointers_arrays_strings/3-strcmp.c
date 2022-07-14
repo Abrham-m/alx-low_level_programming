a3b83ba0ab694178ffe7b17fbeb39db767649325
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-14 15:57:51 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - is afunction that compares two string
+ * @s1: variable
+ * @s2: variable
+ * Return: result
+ */
+
+int _strcmp(char *s1, char *s2);
+{
+	return (strcmp(s1, s2));
+}
