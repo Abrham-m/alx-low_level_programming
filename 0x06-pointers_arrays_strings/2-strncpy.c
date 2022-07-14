05e15a1b9927a21acaa4ff4ee7217711a063da20
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-14 15:53:28 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strncpy - is a function
+ * @dest: is a varible
+ * @src: is a function
+ * @n: is a number
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
