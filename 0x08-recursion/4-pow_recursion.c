13de532c13a7e5357b99acb2f2eb21c42220626f
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-22 16:11:54 -0700

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - is afunction
+ * @x: variable
+ * @y: variable
+ * Return: the result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if return (n * _pow_recursion(x, y - 1));
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1);
+}
