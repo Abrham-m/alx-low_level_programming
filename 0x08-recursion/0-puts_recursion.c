6f585c6faa6bf935a3c9be2807eda92d73ae1813
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-22 00:43:15 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - is a function that prints string
+ * @s: is a variable
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void _putchar(char);
+
+void _puts_recursion(char *s);
+
