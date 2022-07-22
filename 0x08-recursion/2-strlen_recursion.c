00dc26b580f1d6e76a6e678c82f0bdad3438b98e
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-22 02:59:11 -0700

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - main - check the code
+ * @s: is avariable
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-
 void _putchar(char);
 
 void _puts_recursion(char *s);
 
 void _print_rev_recursion(char *s);
 
-
+int _strlen_recursion(char *s);
