30a5c0b0d3b1e92efdae7432a156e13764305514
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-23 04:54:21 -0700

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - is a function that returns radican answers
+ * @n: is a number
+ * Return: 0 or the answer
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return ( - 1);
+	else
+		return (SQRT(N));
+}
