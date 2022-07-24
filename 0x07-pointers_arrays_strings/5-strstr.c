160e4ef7018909583f663ed7e65108ec3ae54169
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-24 13:30:34 -0700

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - is afunction
+ * @haystack: pointer
+ * @needle: pointer
+ * Return: the answer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
