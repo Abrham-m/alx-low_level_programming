9ef1c3bb5c77302f8d5ec237c93ab81f853a1cb9
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-24 13:09:37 -0700

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - is a function
+ * @s: is a pointer
+ * @accept: apointer
+ * Return: the result
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
