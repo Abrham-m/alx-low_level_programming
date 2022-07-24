91fcf467626b0b39efb3e4c395e39dba90ad08be
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-24 02:59:15 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,10 +10,12 @@ int is_prime_number(int n)
 {
 	if (n == 2 || n == 3 || n == 5)
 		return (1);
-	else if (n % 2 == 0)
+	else if (n % 2 == 0 || n == 1)
 		return (0);
 	else if (n % 3 == 0)
 		return (0);
 	else if (n % 5 == 0)
 		return (0);
+	else
+		return (1);
 }
