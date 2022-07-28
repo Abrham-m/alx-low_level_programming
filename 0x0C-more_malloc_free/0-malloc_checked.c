37be6751b36e53c368d4af48e2560939aa1c73bb
Abrham-m <92591172+Abrham-m@users.noreply.github.com>
2022-07-28 22:50:15 +0300

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,17 @@
 #include "main.h"
 
 /**
- *
+ * malloc_checked - is
+ * @b: integer
  */
 
-	if (mallocc(b) == NULL)
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+	
+	*p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
+	
