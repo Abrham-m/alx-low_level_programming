1e57ccc708825752c5a626e84209ba81c1590f77
Abrham_Mengistu <abrhammen14@gmail.com>
2022-07-30 15:49:31 -0700

    static libarary

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,8 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
