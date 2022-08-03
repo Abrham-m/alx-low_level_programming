d8a088b934bdc19d890396324c9248e391ef28e1
Abrham_Mengistu <abrhammen14@gmail.com>
2022-08-03 12:09:34 -0700

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,13 +5,15 @@
  * array_iterator - is a function
  * @array: is
  * @size: is
- * @action:
+ * @action: is
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
+	if (array == NULL || action == NULL)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
