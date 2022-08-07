65cacba4ed86283c151463ef2464f31c749610ba
Abrham_Mengistu <abrhammen14@gmail.com>
2022-08-07 04:56:25 -0700

    0

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,9 +22,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		ptr = va_arg(vl, char *);
 		if (ptr == NULL)
 			printf("nel");
-		printf(ptr);
+		printf("%s",ptr);
 		ptr == NULL;
 		if (i != n - 1)
-			printf(separator);
+			printf("%s",separator);
 	}
 }
